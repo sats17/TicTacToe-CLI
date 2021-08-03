e735c5f56bb56853ed37d0c89ebc7a24f07a3807
Satish Kumbhar <satish.a.kumbhar@capgemini.com>
2021-08-03 21:38:53 +0530

    Updated name from user to player

--- Frontend/GamePlay.py
+++ Frontend/GamePlay.py
@@ -8,24 +8,23 @@ def main():
 
 
 if __name__ == "__main__":
-    # Change user to player
-    user1 = dict()
-    name = input("Enter user one name: ")
+    player1 = dict()
+    name = input("Enter player one name: ")
     value = input("Pick X or O: ")
     if value.lower() not in ['x', 'o']:
         print("Please enter either X or O")
         exit()
-    user1['name'] = name
-    user1['value'] = value.lower()
-    user2 = dict()
-    name = input("Enter user two name: ")
-    user2['name'] = name
-    if user1.get('value') == 'x':
-        user2['value'] = 'o'
-    if user1.get('value') == 'o':
-        user2['value'] = 'y'
-    print("Users information \n", "User 1 : ", user1.get('name'), " ", user1.get('value'), "\n User 2 : ",
-          user2.get('name'), " ", user2.get('value'))
+    player1['name'] = name
+    player1['value'] = value.lower()
+    player2 = dict()
+    name = input("Enter player two name: ")
+    player2['name'] = name
+    if player1.get('value') == 'x':
+        player2['value'] = 'o'
+    if player1.get('value') == 'o':
+        player2['value'] = 'y'
+    print("Players information \n", "Player 1 : ", player1.get('name'), " ", player1.get('value'), "\n Player 2 : ",
+          player2.get('name'), " ", player2.get('value'))
     boardSize = input("Enter board size: ") # Get int and handle error here
     if not isinstance(boardSize, int) or boardSize <= 0 or boardSize > 5:
         print("Please enter only number which is greater than 0 and less than 5")
